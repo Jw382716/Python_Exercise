83d1397fcfaffacc198c46f7339215aab498e441
Jw382716 <92716416+Jw382716@users.noreply.github.com>
2022-01-13 15:32:23 +0900

    Add files via upload

---
+++ othernum_sum.py
@@ -0,0 +1,11 @@
+def solution(numbers):
+    answer = 0
+    num = [0,1,2,3,4,5,6,7,8,9]
+    for i in num :
+        if i not in numbers :
+            answer += i
+    return answer
+
+numbers = [1,2,3,4,6,7,8,0]
+answer = solution(numbers)
+print(answer)
