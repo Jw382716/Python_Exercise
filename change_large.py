771d0f64a586affef6ff3595b4e79a9dcb90bcb0
Jw382716 <92716416+Jw382716@users.noreply.github.com>
2022-01-14 15:23:02 +0900

    Add files via upload

---
+++ change_large.py
@@ -0,0 +1,18 @@
+def solution(k, a, b):
+    answer = 0
+    a.sort()
+    b.sort(reverse=True)
+    
+    for i in range(k) :
+        a[i] = b[i]
+
+    for i in a :
+        answer += i
+
+    return answer
+
+k = 3
+a = [1,2,5,4,3]
+b = [5,5,6,6,5]
+answer = solution(k, a, b)
+print(answer)
